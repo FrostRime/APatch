c973ec670e17fd9af0cd9b31aba1b86f15d14a4a
FrostRime <68203511@qq.com>
2025-12-28 20:03:25 +0800

    test10

--- app/src/main/java/me/bmax/apatch/ui/screen/Home.kt
+++ app/src/main/java/me/bmax/apatch/ui/screen/Home.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -495,7 +496,7 @@ private fun KStatusCard(
             }
         }
 
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(modifier = Modifier.fillMaxWidth().height(intrinsicSize = IntrinsicSize.Max)) {
         ElevatedCard(
             modifier = Modifier
                 .aspectRatio(1f)
